abfb281f6fcb61137e76d1ba103da913aef49aff
christop <christoph@lafkon.net>
2014-09-26 16:53:52 +0200

    read from list

--- backup.sh
+++ backup.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
-  URL=http://pad.constantvzw.org/p/conversations.jhaltiwanger/export/txt
+
   BACKUPDIR=backup
   TMPDIR=.
+  URLS=urls.list
+# URL=http://pad.constantvzw.org/p/conversations.clilley/export/txt
 
 
+ for URL in `cat $URLS`
+  do
+    NAME=`echo $URL | \
+          sed 's,^http://,,g' | \
+          sed 's/\./XjcuDCHX34/g'  | \
+          sed 's,/export/txt,,g'   | \
+          sed 's,/,835FtxcuzG,g'  | \
+          sed 's/[^a-zA-Z0-9 ]//g' | \
+          sed 's,835FtxcuzG,-,g'  | \
+          sed 's/XjcuDCHX34/\./g'`
+ 
+    BACKUPFILE=$BACKUPDIR/${NAME}.txt
+    EPDUMPFILE=${TMPDIR}/${NAME}.tmp
   
-  NAME=`echo $URL | rev | cut -d "/" -f 3 | rev`
-  BACKUPFILE=$BACKUPDIR/${NAME}.txt
-  EPDUMPFILE=${TMPDIR}/${NAME}.tmp
+    if [ -f $BACKUPFILE ]; then
+  
+         MD5BACKUP=`md5sum $BACKUPFILE | cut -d " " -f 1` 
+    else
+         MD5BACKUP=XX  
+         touch $BACKUPFILE
+         git add $BACKUPFILE 
+    fi
+  
+    wget --no-check-certificate -O ${TMPDIR}/${NAME}.tmp $URL > /dev/null 2>&1
+    MD5EPDUMP=`md5sum $EPDUMPFILE | cut -d " " -f 1`
+  
+    if [ $MD5BACKUP != $MD5EPDUMP ]; then
+  
+         mv $EPDUMPFILE $BACKUPFILE
+         GITM="$URL backed up on "`date +%d.%m.%Y\ %H:%M:%S`
+         echo $GITM
+         git commit -m "$GITM" $BACKUPFILE
+  
+    else
+         echo "${NAME}: Backup up-to-date"
+         rm $EPDUMPFILE
+    fi
 
-  if [ -f $BACKUPFILE ]; then
 
-       MD5BACKUP=`md5sum $BACKUPFILE | cut -d " " -f 1` 
-  else
-       MD5BACKUP=XX  
-  fi
-
-  echo $MD5BACKUP
-
-  wget --no-check-certificate -O ${TMPDIR}/${NAME}.tmp $URL > /dev/null 2>&1
-
-  MD5EPDUMP=`md5sum $EPDUMPFILE | cut -d " " -f 1`
-  echo $MD5EPDUMP
-
-  if [ $MD5BACKUP != $MD5EPDUMP ]; then
-
-       mv $EPDUMPFILE $BACKUPFILE
-       GITM="$URL backed up on "`date +%d.%m.%Y`
-
-
-  else
-       echo "$NAME already updated"
-       rm $EPDUMPFILE
-  fi
+ done
 
 
 
